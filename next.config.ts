3618c0c3a8c6df279a3fadb91e40037907385e8b
dejesusbg <dejesusbg5@gmail.com>
2025-10-20 09:18:00 -0500

    add caching for images

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,19 @@ const nextConfig: NextConfig = {
 	eslint: {
 		ignoreDuringBuilds: true,
 	},
+	async headers() {
+		return [
+			{
+				source: '/(.*)\\.(png|jpg|jpeg|webp)',
+				headers: [
+					{
+						key: 'Cache-Control',
+						value: 'public, max-age=31536000, immutable',
+					},
+				],
+			},
+		];
+	},
 };
 
 export default nextConfig;
